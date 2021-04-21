4baa8dc78d9ad8d7cf29c26e6cd55428afea8da2
Marcelo Carvalho <mpirescarvalho17@gmail.com>
2021-04-21 15:39:41 -0300

    create episode page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,18 @@
 import { GetStaticProps } from 'next';
 import Image from 'next/image'
+import Link from 'next/link'
 import { format, parseISO } from 'date-fns'
 import ptBR from 'date-fns/locale/pt-BR'
+
 import { api } from '../services/api';
 import { convertDurationToTimeString } from '../utils/convertDurationToTimeString';
 
-import styles from '../styles/home.module.scss'
+import styles from './home.module.scss'
 
 type Episode = {
   id: string
   title: string
   thumbnail: string
-  description: string
   members: string
   duration: number
   durationAsString: string
@@ -42,7 +43,9 @@ export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
               />
 
               <div className={styles.episodeDetails}>
-                <a href="">{episode.title}</a>
+                <Link href={`/episodes/${episode.id}`}>
+                  <a>{episode.title}</a>
+                </Link>
                 <p>{episode.members}</p>
                 <span>{episode.publishedAt}</span>
                 <span>{episode.durationAsString}</span>
@@ -81,7 +84,9 @@ export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
                   />
                 </td>
                 <td>
-                  <a href="">{episode.title}</a>
+                  <Link href={`/episodes/${episode.id}`}>
+                    <a>{episode.title}</a>
+                  </Link>
                 </td>
                 <td>{episode.members}</td>
                 <td style={{ width: 100 }}>{episode.publishedAt}</td>
@@ -118,7 +123,6 @@ export const getStaticProps: GetStaticProps = async (ctx) => {
     publishedAt: format(parseISO(episode.published_at), 'd MMM yy', { locale: ptBR }),
     duration: Number(episode.file.duration),
     durationAsString: convertDurationToTimeString(Number(episode.file.duration)),
-    description: episode.description,
     url: episode.file.url,
   }))
 
